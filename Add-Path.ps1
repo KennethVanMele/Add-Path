c7340a7eacf21cb53af394e43819180a7d4070c8
Kenneth <kennethvanmele@hotmail.com>
2018-11-15 08:56:01 +0100

    ready for testing

--- Add-Path.ps1
+++ Add-Path.ps1
@@ -1,17 +1,27 @@
-Set-ExecutionPolicy Unrestricted
-$newPath=Read-Host "Folder path to add"
-$regex='^[c-zC-Z]:\\.'
-if ($newPath -notmatch $regex)
-{
-    Write-Host "Forgot drive letter?"
-}
-elif (Test-Path $newPath)
-{
-    $oldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-    $newPath=$oldPath + ';' + $newPath
-    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
-}
-else
-{
-    Write-Host "The folder you want to add doesn't exist."
+function Add-Path {
+    [CmdletBinding()]
+    Param (
+        [parameter(
+            Position = 0,
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            HelpMessage = "Path you want to add.")]
+        [string]$PathName
+     )
+
+    $regex='^[c-zC-Z]:\\.'
+    if ($PathName -notmatch $regex)
+    {
+        Write-Host "Forgot drive letter?"
+    }
+    elif (Test-Path $PathName)
+    {
+        $oldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+        $newPath=$oldPath + ';' + $PathName
+        Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
+    }
+    else
+    {
+        Write-Host "The folder you want to add doesn't exist."
+    }
 }
